1f0f56a66c6f5801e45efaca11ead200b7b23399
Your Name <you@example.com>
2019-03-22 14:58:21 +0800

    addnoise done

--- Ideal Binary Mask/IdealBinaryMask.jl
+++ Ideal Binary Mask/IdealBinaryMask.jl
@@ -1,3 +1,15 @@
+using LinearAlgebra
+using WAV
 function addnoise(signal,noise,snr)
-    SNR = @(signal,noisy)( 20*log10(norm(signal)/norm(signal-noisy)) );    
+    SNR(signal,noisy)=20*log10(norm(signal)/norm(signal-noisy))
+    S=length(signal)
+    N=length(noise)
+    @assert N>S "noise should be longer than signal"  
+    R = rand(1:1+N-S)
+    noise = noise(R:R+S-1)
+    noise = noise / norm(noise) * norm(signal) / 10.0^(0.05*snr)
+    noisy = signal + noise;
+    return noisy,noise
 end
+
+signal=wavread("Ideal Binary Mask/matlab/sp10.wav")
