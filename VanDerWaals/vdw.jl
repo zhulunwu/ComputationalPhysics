2bf51e6f3ddcc5753c95a8afcf2f78621db7b0cc
Your Name <you@example.com>
2019-03-25 10:42:41 +0800

    update

--- VanDerWaals/vdw.jl
+++ VanDerWaals/vdw.jl
@@ -6,14 +6,10 @@ const R=8.314472
 const a_H2O=0.5535
 const b_H2O=0.0305
 const P0=1.01325e5
+const Tc=8a_H2O/(27R*b_H2O)
 
-T=300
-V=0.02:0.001:0.024
-p=(R*T)./V
-pp=p./P0
-plot(pp,v)
-
-
+T=373.15
+V=0.02:0.001:0.032
 p=(R*T)./(V.-b_H2O)-a_H2O./(V.^2)
 pp=p./P0
-plot(p,v)
+plot(V,pp)
