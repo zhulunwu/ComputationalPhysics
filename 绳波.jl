431f4cff05bec390f35aa329c0fe37ed45189e54
zhulunwu <zhulunwu@outlook.com>
2020-03-20 23:30:00 +0800

    更新波动方程写法

--- 绳波.jl
+++ 绳波.jl
@@ -1,18 +1,4 @@
 using GR
-
-# 热传导方程（教材332）
-x=0:20; a2=10;  r=a2*0.01;
-u=zeros(21,25);
-u[10:11,1].=1;
-for j=1:24
-    u[2:20,j+1]=(1-2*r)*u[2:20,j]+r*(u[1:19,j]+u[3:21,j]);
-    plot(x,u[:,j]);
-    ylim([0,1]);
-    sleep(0.1);
-end
-surface(u)
-
-# 绳波方程
 function rope_wave()
     T=0.8
     ρ=0.2
@@ -40,3 +26,4 @@ function rope_wave()
         sleep(0.1)
     end
 end
+rope_wave()
