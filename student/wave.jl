ed39fe1b2fe9861c0d1d307a5304c38c2fdd0cf1
zhulunwu <zhulunwu@outlook.com>
2020-03-26 19:05:11 +0800

    添加一些注释

--- student/wave.jl
+++ student/wave.jl
@@ -85,22 +85,24 @@ absorb_interplot()
 
 # 神经网络方法
 using Flux
-m=Dense(2,1)
-loss(x,y)=Flux.mse(m(x),y)
-opt = ADAM(0.0001)
+m=Dense(2,1)                #神经网络模型，这里仅用到一个神经元，有两个输入，一个输出。未设置激活函数。
+loss(x,y)=Flux.mse(m(x),y)  #损失函数即l=(y-ŷ)^2,ŷ=m(x)是神经网络的输出。
+opt = ADAM(0.0001)          #神经网络训练算法
 # 从已知的波动方程数据中学习y=cos(ωt-kx+ϕ)
 tspan=0:τ:2000τ
+# 训练神经网络
 for n=1:20000
-    ϕ=rand()*π
-    t=rand(tspan)
-    x=rand(X)
-    y1=cos(ω*t-k*x+ϕ)
-    y2=cos(ω*t-k*(x+h)+ϕ)
-    y3=cos(ω*(t+τ)-k*(x+2h)+ϕ)
-    data=[([y1,y2],y3)]
+    ϕ=rand()*π                  #随机设置初始相位
+    t=rand(tspan)               #随机选择时间
+    x=rand(X)                   #随机选择x坐标
+    y1=cos(ω*t-k*x+ϕ)           #t时刻质点x的位移
+    y2=cos(ω*t-k*(x+h)+ϕ)       #t时刻质点x+h的位移
+    y3=cos(ω*(t+τ)-k*(x+2h)+ϕ)  #t+τ时刻质点x+2h的位移，监督学习中，这相当于答案
+    data=[([y1,y2],y3)]         #神经网络输入数据是y1和y2，目标数据是y3。
     Flux.train!(loss,params(m),data,opt)
-    n%100==0 && @info(loss([y1,y2],y3))
+    n%100==0 && @info(loss([y1,y2],y3)) #当n等于100的整数倍时，输出神经网络的损失函数值
 end
+#训练完毕后神经网络m就可以用于预测了
 
 function absorb_nn()
     y0=zeros(N)                             
